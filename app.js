c2c4f3a0ea4de72f66a8e1f6ab1d7f40d8f276c0
Eugenio Ochoa Lopez <eugenio_oso@hotmail.com>
2015-07-25 13:11:22 +0200

    Clean click function in app.

--- app.js
+++ app.js
@@ -7,10 +7,10 @@
  *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *	copies of the Software, and to permit persons to whom the Software is
  *	furnished to do so, subject to the following conditions:
- *	
+ *
  *	The above copyright notice and this permission notice shall be included in all
  *	copies or substantial portions of the Software.
- *	
+ *
  *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -35,30 +35,30 @@ var objectsSelected = {};
 var loadTemplates = function(){
 	itemTemplate = $('#itemTemplate').html();
 	Mustache.parse(itemTemplate);
-	
+
 	gameTemplate = $('#gameTemplate').html();
 	Mustache.parse(gameTemplate);
-	
+
 	navTemplate = $('#navTemplate').html();
 	Mustache.parse(navTemplate);
-	
+
 	//Delete html templates.
 	$('#templates').html('');
-	
+
 	//loadObjects
 	for(var i=0, len = data.cardTypes.length; i < len; i++) {
-		
+
 		typesById["cardTypes-"+data.cardTypes[i].id] = data.cardTypes[i];
-		
+
 		for( var j=0, len1 = data.cardTypes[i].cards.length; j < len1; j++){
 			objectsById["card-"+data.cardTypes[i].cards[j].id] = data.cardTypes[i].cards[j];
 			data.cardTypes[i].cards[j]["cardType"] = data.cardTypes[i];
 		}
-		
+
 	}
-	
+
 	vista["playerPoints"] = $('#game-player-points');
-	
+
 }
 
 var renderItem = function(item){
@@ -76,28 +76,29 @@ var renderNav = function(nav){
 var mostrarType = function(id){
 	$('.cardTypes').css('display', 'none');
 	$('#cardTypes-'+id).css('display', 'block');
-	
+
 	$('.navLink').removeClass('navLink-seleccionado');
 	$('#navLink-cardTypes-'+id).addClass('navLink-seleccionado');
 }
 
 
 //This horrible monstrosity needs an update.
+// Some cleaning done.
 var click = function(id) {
-	
+
 	var obj = objectsById[id];
 	var minPoints = 0,
 		points = data.game.player.points,
 		value = obj.value;
-		
+
 	if( obj["cardType"].isOnlyOne ){
 		for( var key in objectsSelected){
 			if( objectsSelected.hasOwnProperty(key) ){
 				if( objectsSelected[key]["cardType"] == obj["cardType"] &&
 					objectsSelected[key] != obj){
-					
+
 					var oldObject = objectsSelected[key];
-					
+
 					if ( !obj.selected ){
 						if( oldObject.value > 0 ){
 							if( minPoints > points - oldObject.value + value){
@@ -120,16 +121,16 @@ var click = function(id) {
 			}
 		}
 	}
-		
+
 	if( minPoints <= points + value && !obj.selected ){
-		
+
 		$('#'+id).addClass("card-selected");
 		objectsSelected[id] = obj;
-		
+
 		data.game.player.points += value;
 		$('#game-player-points').text(data.game.player.points);
-		
-				
+
+
 		obj.selected = true;
 	}else if ( obj.selected ){
 		if ( value > 0 ) {
@@ -141,9 +142,9 @@ var click = function(id) {
 		$('#'+id).removeClass("card-selected");
 		delete objectsSelected[id];
 		data.game.player.points -= value;
-		
+
 		$('#game-player-points').text(data.game.player.points);
-		
+
 		obj.selected = false;
 	}else{
 		obj.selected = false;
@@ -152,17 +153,17 @@ var click = function(id) {
 
 $(document).ready(function(){
 	loadTemplates();
-	
-	
-	
+
+
+
 	$('#game').html(renderGame(data));
 	$('#cards').append(renderItem(data));
 	$('#nav').append(renderNav(data));
-	
+
 	mostrarType(typesById[Object.keys(typesById)[0]].id);
-	
+
 	$('.card').click(function(){
 		click($(this).attr('id'));
 	});
-	
+
 });
